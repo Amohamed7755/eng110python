0781d301b2a30d52c4e2fb2100d5d4171a617b39
Ahmed mohamed <amohamed@spartaglobal.com>
2022-04-08 13:48:32 +0100

    verified

---
+++ variables/string_intro.py
@@ -0,0 +1,6 @@
+name = input('whats you name?')
+favourite_color = input('whats your favourite color?')
+age = input('what your age?')
+
+print(f'My name is {name} and my favourite color is {favourite_color}, i am {age} years old')
+print(type(age))
---
+++ variables/variables_intro.py
@@ -0,0 +1,20 @@
+# a = 1
+# b = 2
+# c = 3.5
+# d = "hello"
+#
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+a = 125
+b = 1.25
+c = 3 + 4j
+
+print(c, type(c))
+
+
+
+
+
